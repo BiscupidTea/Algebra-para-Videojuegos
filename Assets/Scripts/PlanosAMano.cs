2c10ff6cc556b8ee4f4771f3c11b226b25a6f381
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-05-10 18:54:52 -0300

    add functions of plano a mano

--- Assets/Scripts/PlanosAMano.cs
+++ Assets/Scripts/PlanosAMano.cs
@@ -14,6 +14,7 @@ namespace CustomMatematic
         public Vec3 vertC;
 
         public float distance;
+        public Plano flipped => new(-normal, -distance);
 
         #region Constructors
 
@@ -54,6 +55,65 @@ namespace CustomMatematic
         public static bool operator !=(Plano left, Plano right)
         {
             return !(left == right);
+        }
+        public static Plano Zero { get { return new Plano(Vec3.Zero, 0); } }
+
+        #endregion
+
+        #region Functions
+
+        public Vec3 ClosestPointOnPlane(Vec3 point)
+        {
+            var pointPlaneDistance = Vec3.Dot(point, normal) + distance;
+            return point - (normal * pointPlaneDistance);
+        }
+
+        public void Flip()
+        {
+            this.normal = -normal;
+            this.distance = -distance;
+        }
+
+        public float GetDistanceToPoint(Vec3 point)
+        {
+            return Vec3.Dot(point, normal) + distance;
+        }
+
+        public bool GetSide(Vec3 point)
+        {
+            if (Vec3.Dot(normal, point) + distance > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool SameSide(Vec3 point1, Vec3 point2)
+        {
+            float distance1 = GetDistanceToPoint(point1);
+            float distance2 = GetDistanceToPoint(point2);
+
+            return (distance1 > 0.0f && distance2 > 0.0f) || (distance1 <= 0.0f && distance2 <= 0.0f);
+        }
+
+        public void Set3Points(Vec3 a, Vec3 b, Vec3 c)
+        {
+            this.normal = Vec3.Cross(b - a, c - a).normalized;
+            this.distance = -Vec3.Dot(this.normal, a);
+        }
+
+        public void SetNormalAndPosition(Vec3 inNormal, Vec3 point)
+        {
+            this.normal = Vec3.Cross(inNormal, point);
+            this.distance = 0f + Vec3.Dot(inNormal, point);
+        }
+
+        public void Translate(Vec3 translate)
+        {
+            distance += Vec3.Dot(normal, translate);
         }
 
         #endregion
