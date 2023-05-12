eb900b59aa29b18137d7bf67ac51c3f123a8c1c0
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-05-11 22:02:34 -0300

    add change collor when is colliding

--- Assets/Scripts/Ejercicio 4/Comparador.cs
+++ Assets/Scripts/Ejercicio 4/Comparador.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Comparador : MonoBehaviour
 {
     [SerializeField] private ObjectCollider[] meshes;
+    [SerializeField] private Material trueCollision;
+    [SerializeField] private Material falseCollision;
+    private bool IsColliding;
 
     void Update()
     {
+        IsColliding = false;
         foreach (var point in meshes[0].insidePoints)
         {
             if (meshes[1].ComparePoint(point))
             {
                 Debug.Log("is colliding");
+                IsColliding = true;
             }
+        }
+
+        if (IsColliding)
+        {
+            meshes[0].GetComponent<MeshRenderer>().material = trueCollision;
+            meshes[1].GetComponent<MeshRenderer>().material = trueCollision;
+        }
+        else
+        {
+            meshes[0].GetComponent<MeshRenderer>().material = falseCollision;
+            meshes[1].GetComponent<MeshRenderer>().material = falseCollision;
         }
     }
 }
